dfb1d90d27948c6e99c554cd147246dcd613dcf7
shayrin0 <shayrin.panahi@gmail.com>
2023-05-09 14:15:15 -0400

    Add a java project

---
+++ Stars/src/displayStars.java
@@ -0,0 +1,9 @@
+public class displayStars {
+    public static void main(String args[]) {
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println("*   *");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+    }
+}
