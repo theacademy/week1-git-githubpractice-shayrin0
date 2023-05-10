2f617e126f8db4cc24d303b0e107f01b7be49eaa
shayrin0 <shayrin.panahi@gmail.com>
2023-05-10 13:43:11 -0400

    initial commit

---
+++ Day3_java_basics/src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // block1: declare and initialization
+        String myName = "Shirin";
+        int age = 20;
+        char grade = 'A';
+
+        // block2: calculate and process the data
+
+        // block3: results
+
+        // block4: display the output
+        System.out.println("Welcome to DI003, my name is " + myName);
+    }
+}
