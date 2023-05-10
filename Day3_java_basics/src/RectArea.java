e2c8bab0e474638b0c8b2c1d020a4b545c0a0a6d
shayrin0 <shayrin.panahi@gmail.com>
2023-05-10 13:51:35 -0400

    Add a class to calculate area of a rectangle

---
+++ Day3_java_basics/src/RectArea.java
@@ -0,0 +1,9 @@
+public class RectArea {
+    public static void main(String[] args) {
+        float height=12.5f, width=6.7f, area=0;
+
+        area = height * width;
+
+        System.out.println("The area of the rectangle is " + area);
+    }
+}
